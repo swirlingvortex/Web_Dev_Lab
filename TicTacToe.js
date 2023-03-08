074371590cd7374bf97e696c5c0bc687aadc2f6b
swirlingvortex <goktugberatbas@gmail.com>
2023-03-08 10:35:31 -0500

    first commit

---
+++ TicTacToe.js
@@ -0,0 +1,13 @@
+button1 = document.querySelector("#onetone")
+button1.addEventListener("click", )
+button2 = document.querySelector("#onettwo")
+button3 = document.querySelector("#onetthree")
+button4 = document.querySelector("#twotone")
+button5 = document.querySelector("#twottwo")
+button6 = document.querySelector("#twotthree")
+button7 = document.querySelector("#threetone")
+button8 = document.querySelector("#threettwo")
+button9 = document.querySelector("#threetthree")
+function AddImage() {
+
+}
